160b4b4c50b6a6fc7069ccb639f55690b6015b98
root <root@cybnode11.local>
2017-01-22 10:47:34 +0200

    add prints in main

--- protocols/GMW/main.cpp
+++ protocols/GMW/main.cpp
@@ -10,9 +10,10 @@ int main(int argc, char* argv[]) {
 
     int id = atoi(argv[1]);
     auto parties = MPCCommunication::setCommunication(io_service, id, circuit->getNrOfParties(), argv[3]);
-
+    cout << "----------end communication--------------" << endl;
     string tmp = "init times";
     byte tmpBytes[20];
+    cout << "parties size : " <<parties.size() << endl;
     for (int i=0; i<parties.size(); i++){
         if (parties[i]->getID() < id){
             parties[i]->getChannel()->write(tmp);
@@ -23,9 +24,12 @@ int main(int argc, char* argv[]) {
         }
     }
     int numThreads = atoi(argv[5]);
+    cout << "num of threads : " << numThreads << endl;
     GMWParty party(id, circuit, parties, numThreads, argv[4]);
+    cout << "----------start protocl--------------" << endl;
     party.run();
 
+    cout << "----------finish protocl--------------" << endl;
     return 0;
 }
 
