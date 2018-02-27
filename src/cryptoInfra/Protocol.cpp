ad7593367d9b8d61b5566ab055326d1375d27806
Lior Koskas <liork.cryptobiu@gmail.com>
2018-02-27 10:22:51 +0200

    Update Protocol API

--- src/cryptoInfra/Protocol.cpp
+++ src/cryptoInfra/Protocol.cpp
@@ -3,34 +3,60 @@
 //
 #include <../../include/cryptoInfra/Protocol.hpp>
 
-string CmdParser::getKey(string parameter){
-    if (parameter[0] == '-'){
+string CmdParser::getKey(string parameter)
+{
+    if (parameter[0] == '-')
         return parameter.substr(1);
-    } else return parameter;
+    else
+        return parameter;
 }
 
-map<string, string> CmdParser::parseArguments(string protocolName, int argc, char* argv[]){
-    map<string, string> arguments;
 
-    string key;
+string CmdParser::getValueByKey(vector<pair<string, string>> arguments, string key)
+{
+    int size = arguments.size();
+    for (int i = 0; i < size; ++i)
+    {
+        pair<string, string> p = arguments[i];
+        if (p.first == key)
+            return p.second;
+    }
+    return "NotFound";
+}
 
-    //Put the protocol name in the map
-    arguments["protocolName"] = protocolName;
+vector<pair<string, string>> CmdParser::parseArguments(string protocolName, int argc, char* argv[])
+{
+    string key, value;
+
+    //Put the protocol name in the vector pairs
+    vector<pair<string, string>> arguments;
+    arguments.push_back(make_pair("protocolName", protocolName));
 
     //Put all other parameters in the map
-    for(int i=1; i<argc; i+=2){
+    for(int i=1; i<argc; i+=2)
+    {
 
         key = getKey(string(argv[i]));
-        arguments[key] = argv[i+1];
+        value = getKey(string(argv[i+1]));
+        arguments.emplace_back(make_pair(key, value));
 
-        cout<<"key = "<<key<<" value = "<<arguments[key]<<endl;
+        cout<<"key = "<< key <<" value = "<< value <<endl;
     }
 
     return arguments;
-};
-
+}
 
 Protocol::Protocol(string protocolName, int argc, char* argv[])
 {
     arguments = parser.parseArguments(protocolName, argc, argv);
+}
+
+vector<pair<string, string>> Protocol::getArguments()
+{
+    return arguments;
+}
+
+CmdParser Protocol::getParser()
+{
+    return parser;
 }
